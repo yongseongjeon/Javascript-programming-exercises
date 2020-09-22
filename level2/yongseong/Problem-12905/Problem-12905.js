1dc8f5f1141bcef09cdeec86903c656c89af65e5
yongseongjeon <yongseong.jeon95@gmail.com>
2020-09-22 20:14:04 +0900

    Modified problem 12905

--- level2/yongseong/Problem-12905/Problem-12905.js
+++ level2/yongseong/Problem-12905/Problem-12905.js
@@ -1,31 +1,22 @@
 function solution(board) {
   const rowLength = board[0].length;
   const columnLength = board.length;
+  let squareLength = 0;
   
-  // 모두 0인 케이스
-  if (board.flat(1).every((x) => x === 0)) return 0;
-  
-  for (let i = Math.min(rowLength, columnLength); i >= 1; i--) {
-      let squareArea = getSquareArea(i);
-      if (squareArea) return squareArea;
-  }
-
-  function getSquareArea(suqareLength) {
-      for (let i = 0; i <= rowLength - suqareLength; i++) {
-          for (let j = 0; j <= columnLength - suqareLength; j++) {
-              if (isPossibleSquare(i, j, suqareLength)) {
-                  return suqareLength * suqareLength;
-              }
+  for (let i = 1; i < columnLength; i++) {
+      for (let j = 1; j < rowLength; j++) {
+          if (board[i][j] !== 0) {
+              const minimumVal = Math.min(board[i - 1][j - 1], board[i - 1][j], board[i][j - 1]);
+              board[i][j] = minimumVal + 1;
+              if (squareLength < board[i][j]) squareLength = board[i][j];
           }
       }
-      return false;
   }
-  function isPossibleSquare(x, y, length) {
-      for (let i = y; i < y + length; i++) {
-          for (let j = x; j < x + length; j++) {
-              if (board[i][j] === 0) return false;
-          }
-      }
-      return true;
+  
+  if (rowLength < 2 || columnLength < 2) {
+      if (board.flat(1).some((x) => x === 1)) return 1;
+      else return 0;
   }
+  
+  return squareLength ** 2;
 }
